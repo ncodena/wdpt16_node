87e0676f2eecd8409f5d528f4756e35baa6cc789
Nuria <c.odena.nuria@gmail.com>
2023-09-27 19:20:56 +0200

    updating

--- index.js
+++ index.js
@@ -1,58 +1,104 @@
 import express from 'express';
-import {pets} from './petList.js';
+import { body, validationResult } from 'express-validator';
+
 const app = express();
+
+// Parse JSON request bodies
+// This middleware is responsible for parsing incoming request bodies, making it available under req.body.
+app.use(express.json());
 
 const port = 8080 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send(`
-      <h1>Adopt a Pet!</h1>
-      <p>Browse through the links below to find your new furry friend:</p>
-      <ul>
-        <li><a href="/animals/dogs">Dogs</a></li>
-        <li><a href="/animals/cats">Cats</a></li>
-        <li><a href="/animals/rabbits">Rabbits</a></li>
-      </ul>
-    `);
+const countries = [
+    { id: 1, name: 'Bhutan', alpha2Code: 'BT', alpha3Code: 'BTN', visited: false },
+    { id: 2, name: 'France', alpha2Code: 'FR', alpha3Code: 'FRA', visited: false },
+    { id: 3, name: 'Japan', alpha2Code: 'JP', alpha3Code: 'JPN', visited: false },
+    { id: 4, name: 'Australia', alpha2Code: 'AU', alpha3Code: 'AUS', visited: false },
+    { id: 5, name: 'South Africa', alpha2Code: 'ZA', alpha3Code: 'ZAF', visited: false }
+];
+
+const countryValidation = [
+    body('name').isString().optional(),
+    body('alpha2Code').isLength({ min: 2, max: 2 }).optional(),
+    body('alpha3Code').exists().isLength({ min: 3, max: 3 }).optional(),
+    body('visited').isBoolean().optional()
+];
+
+app.get('/api/countries', (req, res) => {
+    let responseCountries = countries;
+    if (req.query.sort === "true") {
+        responseCountries = [...countries].sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (req.query.visited === 'true') {
+        responseCountries = responseCountries.filter(country => country.visited === true);
+    }
+    res.json(responseCountries);
 });
 
 
-
-app.get('/animals/:pet_type', (req, res) => {
-    const petType = req.params.pet_type;
-    const petList = pets[petType];
-    
-    let content = `<h1>List of ${petType}</h1>`;
-    content += '<ul>';
-    
-    petList.forEach((pet, index) => {
-      content += `<li><a href="/animals/${petType}/${pet.name}">${pet.name}</a></li>`;
-    });
-    
-    content += '</ul>';
-    
-    res.send(content);
-  });
-
-  app.get('/animals/:pet_type/:pet_name', (req, res) => {
-    const petType = req.params.pet_type;
-    const petName = req.params.pet_name;
-    const typePet = pets[petType];
-    const findPet = typePet.find(pet => pet.name.toLowerCase() === petName.toLowerCase());
-    if(!findPet) {
-      res.status(404).send("Pet not found");
-      return;
+app.post('/api/countries', countryValidation, (req, res) => {
+    console.log(req.body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-  
-    let content = `<h1>${findPet.name}</h1>`;
-    content += `<img src="${findPet.url}" alt="${findPet.name}"/>`;
-    content += `<p>${findPet.description}</p>`;
-    content += `<ul><li>Breed: ${findPet.breed}</li><li>Age: ${findPet.age}</li></ul>`;
+
+    const existingCountry = countries.find(country => country.alpha2Code === req.body.alpha2Code || country.alpha3Code === req.body.alpha3Code);
+    if (existingCountry) {
+        return res.status(400).json({ error: "Country already exists" });
+    }
+
+    const newCountry = {
+        id: countries.length + 1,
+        ...req.body
+    };
+    countries.push(newCountry);
+    res.status(201).json(newCountry);
+});
+
+app.get('/api/countries/:code', (req, res) => {
+    const code = req.params.code.toUpperCase();
+    console.log(code, 'code')
+    const country = countries.find(country => country.alpha2Code === code || country.alpha3Code === code);
     
-    res.send(content);
-  });
+    if (!country) {
+        return res.status(404).json({ error: "Country not found" });
+    }
 
+    res.json(country);
+});
 
+app.put('/api/countries/:code', countryValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const code = req.params.code.toUpperCase();
+    const country = countries.find(country => country.alpha2Code === code || country.alpha3Code === code);
+    
+    if (!country) {
+        return res.status(404).json({ error: "Country not found" });
+    }
+    //If a property exists in both country and req.body, the value in country will be overwritten by the value from req.body.
+    //If a property exists only in req.body, it will be added to country.
+    //If a property exists only in country, it will remain unchanged.
+    Object.assign(country, req.body);
+    res.json(country);
+});
+
+app.delete('/api/countries/:code', (req, res) => {
+    const code = req.params.code.toUpperCase();
+    const country = countries.find(country => country.alpha2Code === code || country.alpha3Code === code);
+    
+    if (!country) {
+        return res.status(404).json({ error: "Country not found" });
+    }
+
+    country.visited = !country.visited;
+    res.json(country);
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
