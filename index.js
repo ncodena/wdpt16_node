41138ce867c64d3bc2e62c30c47ef295503d413f
Nuria <c.odena.nuria@gmail.com>
2023-10-09 18:27:17 +0200

    updating code

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ import studentsRouter from './routes/students.js';
 
 const app = express();
 app.use(express.json());
-
-const loggerMiddleware = (request, response, next) => {
-    console.log('Console logging from the middleware!'); // Logs the string and goes to the next function in the pipeline
-    next();								 
-    };
-    const myHandlerFunction = (request, response) => response.send(request.method); // Ends the req/res cycle
-    
-    app.get('/', loggerMiddleware, myHandlerFunction);     // The client will see 'get' in the browser
-    app.post('/', loggerMiddleware, myHandlerFunction);    // The client will see 'post' in the browser
-    app.put('/', loggerMiddleware, myHandlerFunction);     // The client will see 'put' in the browser
-    app.delete('/', loggerMiddleware, myHandlerFunction)
-
 app.use('/api/films', filmsRouter);
 app.use('/api/students', studentsRouter);
 
