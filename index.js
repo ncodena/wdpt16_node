f843e84777f8eae2f41c313ebc5341075c03c1aa
Nuria <c.odena.nuria@gmail.com>
2023-10-12 15:52:22 +0200

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,45 @@ import studentsRouter from './routes/students.js';
 
 const app = express();
 app.use(express.json());
+// const middlewareFunction = (req, res, next) => {
+//     //Perform some action on the request or response
+//     console.log({url: req.originalUrl});
+//     next();
+// }
+
+//app.use(middlewareFunction)
+
+// const getOnlyMiddleware = (request, response, next) => {
+//     if(request.method === 'GET') { // processes the request and calls next if method is GET
+//       next();
+//     } else {
+//      return response.sendStatus(403); // wrong method used, ends req/res cycle with an error message
+//     }
+// };
+
+// app.use('/path', getOnlyMiddleware);
+
+// app.get("/path", async (req, res) => {
+//     try {
+//       res.send('hello')
+//     } catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+const loggerMiddleware = (request, response, next) => {
+    console.log('Console logging from the middleware!'); // Logs the string and goes to the next function in the pipeline
+    next();								  
+};
+
+const myHandlerFunction = (request, response) => response.send(request.method); // Ends the req/res cycle
+    
+app.get('/', loggerMiddleware, myHandlerFunction);     // The client will see 'get' in the browser
+app.post('/', loggerMiddleware, myHandlerFunction);    // The client will see 'post' in the browser
+app.put('/', loggerMiddleware, myHandlerFunction);     // The client will see 'put' in the browser
+app.delete('/', loggerMiddleware, myHandlerFunction);  // The client will see 'delete' in the browser
+
+
 app.use('/api/films', filmsRouter);
 app.use('/api/students', studentsRouter);
 
--- routes/students.js
+++ routes/students.js
@@ -2,6 +2,19 @@ import express from 'express';
 const studentsRouter = express.Router();
 import Student from "../models/Student.js";
 
+const middlewareAuthorizationFunction = (req, res, next) => {
+    //Perform some action on the request or response
+    const {token} = req.body;
+    const secretToken = process.env.SECRET_TOKEN;
+
+    if(!token || secretToken !== token){
+        res.status(403).json({error: 'Unauthorized'});
+    } else {
+        next()
+    }
+}
+
+//studentsRouter.use(middlewareAuthorizationFunction)
 
 studentsRouter.get("/", async (req, res) => {
     try {
@@ -13,42 +26,55 @@ studentsRouter.get("/", async (req, res) => {
     }
 })
 
-studentsRouter.post("/", async (req, res) => {
+
+
+studentsRouter.post("/", middlewareAuthorizationFunction, async (req, res) => {
     const {name, first_name, email} = req.body;
     try {
       const response = await Student.create({name, first_name, email});
       res.status(201).json(response);
 
     } catch(err){
+        console.log(err)
         if (err.code === 11000 && err.keyPattern && err.keyPattern.email) {
             return res.status(400).json({ error: 'Email already exists.' });
         }
         return res.status(500).json(err)
     }
 })
+const middlewareErrorFunction = (err, req, res, next) => {
+    //Perform some action on the request or response
+    console.log(err);
+    const statusCode = err.statusCode || 500;
+    const statusMessage = err.message || "Internal server error";
+    res.status(statusCode).json({error: statusMessage})
+}
 
-studentsRouter.put('/:name', async (req, res) => {
+studentsRouter.put('/:name', middlewareAuthorizationFunction, async (req, res, next) => {
         const {newName} = req.body;
         const {name} = req.params;
     try {
         const result = await Student.updateMany({ name }, { name: newName });
+
         if(result.modifiedCount > 0){
             const updatedDocs = await Student.find({ name: newName });
             res.json(updatedDocs)
         } else {
-            res.status(404).json({message: 'No records match provided criteria'})
+            //res.status(404).json({message: 'No records match provided criteria'})
+            return next({statusCode: 404, message: "No records match provided criteria"})
         }
     } catch (err) {
         console.log(err)
-        res.status(500).send(err);
+        return next()
+
     }
-});
+}, middlewareErrorFunction);
 
 studentsRouter.put("/oneUser/:id", async (req, res) => {
     const {name, first_name, email} = req.body;
     const {id} = req.params;
     try {
-        const result = await Student.findByIdAndUpdate(id, { name, first_name, email } );
+        const result = await Student.findByIdAndUpdate(id, { name, first_name, email }, {new: true} );
         if(!result) {
             res.status(404).json({message: "The resource does not exist"})
           }
