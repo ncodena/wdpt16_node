9d67a31396aa17b6cf7389c6b5c36751b34b8e1b
Nuria <c.odena.nuria@gmail.com>
2023-10-18 16:59:41 +0200

    first commit

--- models/Student.js
+++ models/Student.js
@@ -1,15 +1,8 @@
 import mongoose from "mongoose";
-const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
 
 const StudentSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 100,
-        trim: true
-    },
-    first_name: {
         type: String,
         required: true,
         minlength: 1,
@@ -21,19 +14,15 @@ const StudentSchema = new mongoose.Schema({
         required: [true, "Email required"],
         minlength: 1,
         maxlength: 100,
-        unique: true,
         trim: true,
-        // validate: {
-        //     validator: email => emailRegex.test(email),
-        //     message: props => `${props.value} is not a valid email address!`
-        // },
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
-    tokenId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Token'
+    password: {
+        type: String,
+        required: true
     }
-})
+    
+},  {timestamps: true} )
 
 const Student = mongoose.model('Student', StudentSchema);
 
--- routes/students.js
+++ routes/students.js
@@ -1,23 +1,38 @@
 import express from 'express';
 const studentsRouter = express.Router();
 import Student from "../models/Student.js";
-import Token from '../models/Token.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-const middlewareAuthorizationFunction = (req, res, next) => {
-    //Perform some action on the request or response
-    const {token} = req.body;
-    const secretToken = process.env.SECRET_TOKEN;
+const secret = process.env.SECRET_TOKEN;
 
-    if(!token || secretToken !== token){
-        res.status(403).json({error: 'Unauthorized'});
-    } else {
-        next()
-    }
+const generateToken = (data) => {
+  return jwt.sign(data, secret, {expiresIn: '1800s'})
 }
 
-//studentsRouter.use(middlewareAuthorizationFunction)
+const middlewareAuthorizationFunction = (req, res, next) => {
+    //Get token from header
+    const token = req.headers.authorization;
+    
+    if(!token){
+        return res.sendStatus(401)
+    }
 
-studentsRouter.get("/", async (req, res) => {
+    const tokenData = token.split(' ')[1];
+    console.log(tokenData)
+
+    //Verify token
+
+    jwt.verify(tokenData, secret, (err, user) => {
+        if(err){
+            return res.sendStatus(401)
+        }
+        req.user = user;
+        next();
+    })
+}
+
+studentsRouter.get("/", middlewareAuthorizationFunction, async (req, res) => {
     try {
       const response = await Student.find();
       res.json(response)
@@ -27,106 +42,42 @@ studentsRouter.get("/", async (req, res) => {
     }
 })
 
-const middlewareErrorFunction = (err, req, res, next) => {
-    //Perform some action on the request or response
-    console.log(err);
-    const statusCode = err.statusCode || 500;
-    const statusMessage = err.message || "Internal server error";
-    res.status(statusCode).json({error: statusMessage})
-}
 
-studentsRouter.post("/", async (req, res) => {
-    const {name, first_name, email} = req.body;
+studentsRouter.post("/register", async (req, res) => {
+    const {name, email, password} = req.body;
     try {
-      const response = await Student.create({name, first_name, email});
-      res.status(201).json(response);
+        //Hash the password before saving to DB
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const response = await Student.create({name, email, password: hashedPassword});
+        res.status(201).json(response);
 
     } catch(err){
-        console.log(err)
-        if (err.code === 11000 && err.keyPattern && err.keyPattern.email) {
-            return res.status(400).json({ error: 'Email already exists.' });
-        }
         return res.status(500).json(err)
     }
 })
 
-const secure = (req, res, next) => {
-    const token = req.params.token;
-    if(token && token.length > 3){
-        next()
-    } else {
-        next({statusCode: 403, message: "Forbidden"})
-    }
-}
-
-studentsRouter.post("/create-token/:userId", async (req, res, next) => {
-    console.log('hey there')
-    try{
-        const {tokenData} = req.body
-        //Save a token document to the db
-        const token = await Token.create({value: tokenData});
-        const {userId} = req.params;
-        //Update student document with tokenId value from the previous creation of the token
-        const student = await Student.findByIdAndUpdate(userId, {tokenId: token._id}, {new: true});
-        //If no student is found with the given ID, an error is thrown
-        if(!student) return next({statusCode: 404, message: "User not found"})
-        //Otherwise we send the student
-        res.send(student)
-    } catch(err){
-        next()
-    }
-}, middlewareErrorFunction)
-
-studentsRouter.get("/verify/:token", secure, async (req, res, next) => {
+studentsRouter.post("/login", async (req, res) => {
+    const { email, password} = req.body;
     try {
-     //Check if the token is available in the database
-    const token = await Token.findOne({value: req.params.token});
-    if(!token) return next({statusCode: 401, message: "Invalid token!"})
-    //Check if the token is available on a user
-    const student = await Student.findOne({tokenId: token._id});
-    if(!student) return next({statusCode: 401, message: "Invalid token!"})
-    //if the user is linked to this tokemn
-    res.send("token valid");
-    } catch(err){
-        console.log(err)
-        next()
-    }
-}, middlewareErrorFunction)
-
-studentsRouter.put('/:name', middlewareAuthorizationFunction, async (req, res, next) => {
-        const {newName} = req.body;
-        const {name} = req.params;
-    try {
-        const result = await Student.updateMany({ name }, { name: newName });
-
-        if(result.modifiedCount > 0){
-            const updatedDocs = await Student.find({ name: newName });
-            res.json(updatedDocs)
-        } else {
-            //res.status(404).json({message: 'No records match provided criteria'})
-            return next({statusCode: 404, message: "No records match provided criteria"})
+        //Find user in db
+        const user = await Student.findOne({email});
+        if(!user){
+            return res.status(400).send('Invalid credentials');
         }
-    } catch (err) {
-        console.log(err)
-        return next()
+        //Compare passwords
+        const validPassword = await bcrypt.compare(password, user.password)
+        if(!validPassword){
+            return res.status(400).send('Invalid credentials');
+        }
+        const token = generateToken({email: user.email});
+        res.json({token})
 
-    }
-}, middlewareErrorFunction);
-
-studentsRouter.put("/oneUser/:id", async (req, res) => {
-    const {name, first_name, email} = req.body;
-    const {id} = req.params;
-    try {
-        const result = await Student.findByIdAndUpdate(id, { name, first_name, email }, {new: true} );
-        if(!result) {
-            res.status(404).json({message: "The resource does not exist"})
-          }
-          res.json(result)
 
     } catch(err){
-        res.status(500).json(err)
+        return res.status(500).json(err)
     }
 })
+
 
 studentsRouter.get("/:id", async (req, res) => {
     const {id} = req.params;
@@ -138,23 +89,6 @@ studentsRouter.get("/:id", async (req, res) => {
       res.json(response)
 
     } catch(err){
-        res.status(500).json(err)
-    }
-})
-
-studentsRouter.delete("/:id", async (req, res) => {
-    const {id} = req.params;
-    try {
-        const result = await Student.findByIdAndDelete(id);
-        console.log(result, 'result')
-        if (!result) {
-            return res.status(404).json({ message: "The resource does not exist" });
-        } else {
-            res.json(result);
-        }
-        
-    } catch(err){
-        console.log(err)
         res.status(500).json(err)
     }
 })
